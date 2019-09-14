275a44aa36cf9c5cc3f26a012b2e146f4d3c8d2f
Mladen <mladsdf@sdf.com>
2019-09-14 14:28:58 +0200

    created dummy PostList

--- src/AppRoot.tsx
+++ src/AppRoot.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { PostList } from './PostList';
 
-export const AppRoot = () => {
+export function AppRoot(){
   return (
     <div>
-      AppRoot
+      <PostList />
     </div>
   )
 }
