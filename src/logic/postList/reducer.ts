1c4d6b2d181ee5a86d7fdc6904150ea9842ce9fe
Mladen <mladsdf@sdf.com>
2019-09-17 10:25:29 +0200

    minor

--- src/logic/postList/reducer.ts
+++ src/logic/postList/reducer.ts
@@ -1,8 +1,7 @@
 import { ActionType, createReducer } from 'typesafe-actions';
 import * as a from "./actions";
-import { PostListState, PostDetailsFetchContext, PostBasic, PostListViewData } from './types';
+import { PostListState } from './types';
 import produce from 'immer';
-import { findAndUpdate, AsyncOperationStatus } from '../../utils';
 import { createFetchableDataStateFactory } from '../fetchableState';
 
 type RootAction = ActionType<typeof import('./actions')>;
@@ -16,44 +15,6 @@ const initialState: PostListState = {
 }
 
 export const reducer = createReducer<PostListState, RootAction>(initialState)
-    // .handleAction(a.onFetchPostDetails, (s, action) => produce(s, state => {        
-    //     findAndUpdate(
-    //         state.data!, 
-    //         postContext => postContext.type === 'PostDetailsFetchContext',
-    //         postContext => {
-    //             const casted = postContext as PostDetailsFetchContext
-    //             //take details as basic for the case when some data might have changed on backend
-    //             return casted.status === AsyncOperationStatus.Completed ?
-    //                 {...casted.details!, type: 'PostBasic'} as PostBasic :
-    //                 casted.basic                    
-    //         }
-    //     )
-    //     findAndUpdate(
-    //         state.data!, 
-    //         postContext => postContext.type === 'PostBasic' && postContext.id === action.payload,
-    //         postContext => ({
-    //             type: 'PostDetailsFetchContext',
-    //             basic: postContext as PostBasic,
-    //             details: undefined,
-    //             status: AsyncOperationStatus.Processing
-    //         } as PostDetailsFetchContext)
-    //     )
-    // })) 
-    // .handleAction(a.onFetchPostDetailsSuccess, (s, action) => produce(s, state => {
-    //     const postDetails = action.payload
-    //     findAndUpdate(
-    //         state.data!,
-    //         postContext => 
-    //             postContext.type === 'PostDetailsFetchContext' && 
-    //             postContext.status === AsyncOperationStatus.Processing &&
-    //             postContext.basic.id === postDetails.id,
-    //         postContext => {
-    //             const casted = postContext as PostDetailsFetchContext
-    //             casted.status = AsyncOperationStatus.Completed
-    //             casted.details = postDetails
-    //         }
-    //     );
-    // }))
     .handleAction(a.onPostBasicClick, (s, action) => produce(s, state => {
         state.selectedPostId = action.payload
     })) 
@@ -66,7 +27,7 @@ export const reducer = createReducer<PostListState, RootAction>(initialState)
     .handleAction(a.fetchPostListSuccess, (s, action) => produce(s, state => {
         state.lastListFetch = fetchStateFactory.onComplete(action.payload)
     }))
-    .handleAction(a.fetchPostListFailure, (s,) => produce(s, state => {
+    .handleAction(a.fetchPostListFailure, (s, action) => produce(s, state => {
         state.lastListFetch = fetchStateFactory.onFailure()
     }))
     .handleAction(a.fetchPostList, (s) => produce(s, state => {
--- src/view/AppRoot.tsx
+++ src/view/AppRoot.tsx
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles({
   postList: {
-    padding: '0.6em 2em',
+    padding: '0.6em 1em',
     width: '35em',
   }
 });
